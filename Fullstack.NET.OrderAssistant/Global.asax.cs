601bb3df731fd1d88db5edb096b6c15a3d0b4088
react <vlad.medvedovskiy@gmail.com>
2018-08-12 21:50:30 +0300

    Updated bot to use in-memory store

--- Fullstack.NET.OrderAssistant/Global.asax.cs
+++ Fullstack.NET.OrderAssistant/Global.asax.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
+using Autofac;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
 
 namespace Fullstack.NET.OrderAssistant
 {
@@ -11,6 +10,25 @@ namespace Fullstack.NET.OrderAssistant
     {
         protected void Application_Start()
         {
+            var store = new InMemoryDataStore();
+
+            Conversation.UpdateContainer(
+                       builder =>
+                       {
+                           builder.Register(c => store)
+                                     .As<IBotDataStore<BotData>>()
+                                     .SingleInstance();
+
+                           builder.Register(c => new CachingBotDataStore(store,
+                                      CachingBotDataStoreConsistencyPolicy
+                                      .ETagBasedConsistency))
+                                      .As<IBotDataStore<BotData>>()
+                                      .AsSelf()
+                                      .InstancePerLifetimeScope();
+
+
+                       });
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
